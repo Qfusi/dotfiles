4b711bfe232dfa9a4bb44ae6bab96a93af356d0e
Qfusi <jonathan.isaksson93@gmail.com>
2026-03-03 21:57:31 +0100

    fix(dots): add noctalia-shell, fish and ohmyposh

---
+++ home/dot_config/fish/conf.d/10-aliases.fish
@@ -0,0 +1,41 @@
+# -----------------------------------------------------
+# ALIASES
+# -----------------------------------------------------
+
+# -----------------------------------------------------
+# General
+# -----------------------------------------------------
+alias c='clear'
+alias ff='fastfetch'
+alias ls='eza -a --icons=always'
+alias ll='eza -al --icons=always'
+alias lt='eza -a --tree --level=1 --icons=always'
+alias shutdown='systemctl poweroff'
+alias v='$EDITOR'
+alias vim='$EDITOR'
+alias wifi='nmtui'
+
+# -----------------------------------------------------
+# Window Managers
+# -----------------------------------------------------
+
+alias Qtile='startx'
+# Hyprland with Hyprland
+
+# -----------------------------------------------------
+# Git
+# -----------------------------------------------------
+alias g="git"
+
+# -----------------------------------------------------
+# System
+# -----------------------------------------------------
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias update='~/.config/dots/scripts/installupdates.sh'
+
+# -----------------------------------------------------
+# Qtile
+# -----------------------------------------------------
+alias res1='xrandr --output DisplayPort-0 --mode 2560x1440 --rate 120'
+alias res2='xrandr --output DisplayPort-0 --mode 1920x1080 --rate 120'
+alias setkb='setxkbmap se;echo "Keyboard set back to se."'
---
+++ home/dot_config/fish/config.fish
@@ -0,0 +1,6 @@
+# Start Hyprland at login on TTY1
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec start-hyprland
+    end
+end
